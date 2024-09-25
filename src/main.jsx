1b5309c996e329bd3931f3e299bb04c872522796
marxattax <cuisy.vincent@gmail.com>
2024-09-25 02:06:26 +0200

    up

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
 import ScrollToTop from './scrollToTop.jsx'
 import App from './App.jsx'
 import Header from './components/FixedComponents/Header/index.jsx'
@@ -17,6 +17,6 @@ createRoot(document.getElementById('root')).render(
       </main>
       <Footer />
     </Router>
-  </StrictMode>,
+  </StrictMode>
 )
 
