0f86be184a525e0470f072bfc6dfe6d002419b03
marxattax <cuisy.vincent@gmail.com>
2024-09-25 02:34:58 +0200

    try catch et commentaire

--- src/libraries/functionsAPI.js
+++ src/libraries/functionsAPI.js
@@ -1,23 +1,24 @@
 import { routesApi } from "./routesApi";
 
-export async function getRentals(func) {
-    const response = await fetch(routesApi.getRentals);
-    return response.json()
-    
-    .then(rentals => {
-        return rentals;
-    });
+export async function getRentals() {
+    try {
+        const response = await fetch(routesApi.getRentals);
+        return response.json()
+    } catch (error) {
+        console.error(error);
+    }
+
 }
 
 export async function getOneRental(id) {
     const response = await fetch(`${routesApi.getOneRental}`);
     return response.json()
-
-    .then(rentals => {
-        for(const rental of rentals) {
-            if(rental.id == id) {
-                return rental;
+    /* fonction provisoire en attendant la création du backend */
+        .then(rentals => {
+            for(const rental of rentals) {
+                if(rental.id === id) {
+                    return rental;
+                }
             }
-        }
-    });
+        });
 }
